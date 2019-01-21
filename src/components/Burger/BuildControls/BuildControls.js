372c1b0cd152232724a0b5f7ca475e7e3cf8913a
Adam Pierce <adam.pierc3@gmail.com>
2019-01-21 12:40:10 -0800

    Added build controls for the burger builder

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl'
+import classes from './BuildControls.module.css';
+
+const buildControls = ( props ) => {
+    <div>
+
+    </div>
+
+};
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = ( props ) => {
-    
-    const transformedIngredients = Object.keys(props.ingredients)
+
+    let transformedIngredients = Object.keys(props.ingredients)   //transforms state, which is an Object, into an array of the burger ingredients
         .map(ingred => {
 
             let ingredReturn = [];
 
-            for (let i = 1; i <= props.ingredients[ingred]; i++) {
+            for (let i = 1; i <= props.ingredients[ingred]; i++) {  //loops to add the correct number of ingredients to a 2nd dimensional array
                 ingredReturn.push(<BurgerIngredient key={ingred + i} type={ingred} />)
             }
             return ingredReturn;
         })
+        .reduce((arr, elem) => {                        //this bit of code "flattens" the 2D array into a 1D array
+            return arr.concat(elem)
+        }, []);                                         //initial value is an empty array for the null case
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Add some ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
@@ -24,12 +31,5 @@ const burger = ( props ) => {
         </div>
     );
 };
-
-// <div className={classes.Burger}>
-//             <BurgerIngredient type="bread-top" />
-//             <BurgerIngredient type="cheese" />
-//             <BurgerIngredient type="meat" />
-//             <BurgerIngredient type="bread-bottom" />
-// </div>
 
 export default burger;
