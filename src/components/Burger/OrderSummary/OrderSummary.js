64c2d42454dc1ea01570d9b9fbfbcee14af0ed27
Adam Pierce <adam.pierc3@gmail.com>
2019-01-22 11:08:22 -0800

    Added Order Summary page and functionality

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Ox';
+// import classes from './OrderSummary.module.css'
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.entries(props.ingredients).map(ingred => (
+        <li key={ingred[0]}>
+            <span style={{textTransform: 'capitalize'}}>{ingred[0]}</span>: {ingred[1]}
+        </li>
+    ));
+
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>An okay burger with these ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            
+        </Aux>
+    )
+};
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Ox';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES =  {    //typically you name global constants in all caps
     lettuce: 0.4,
@@ -88,6 +90,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     onAddIngred={this.onAddIngred}
