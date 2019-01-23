ee46d2a8019cbe344eb0d15fd83111c607c70184
Adam Pierce <adam.pierc3@gmail.com>
2019-01-23 11:44:21 -0800

    Corrected styling and side drawer functionality for mobile devices

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css'
-
+                                                        //height: props.height is a way of dynamically setting sizing of a reusable element
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './SideDrawer.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Ox';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.show) {
+        attachedClasses.pop()
+        attachedClasses.push(classes.Open);
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.show} cancel={props.cancel} />
+            <div className={attachedClasses.join(" ")}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+}
+
+export default sideDrawer;
