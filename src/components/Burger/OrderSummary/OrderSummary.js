8b036ea03917304c99fe55ccf56647e639d65c79
Adam Pierce <adam.pierc3@gmail.com>
2019-01-24 11:27:28 -0800

    performance udpates, changed modal and order summary to only update when this.state.show changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Ox';
 import Button from '../../UI/Button/Button';
 // import classes from './OrderSummary.module.css'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.entries(props.ingredients).map(ingred => (
-        <li key={ingred[0]}>
-            <span style={{textTransform: 'capitalize'}}>{ingred[0]}</span>: {ingred[1]}
-        </li>
-    ));
+class OrderSummary extends Component {
+    //this could easily be a functional component, should update
+    componentWillUpdate() {
+        console.log('[OrderSummary] Will Update');
+    }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>An okay burger with these ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button type="Danger" clicked={props.clickCancel}>CANCEL</Button>
-            <Button type="Success" clicked={props.clickContinue}>CONTINUE</Button>
-        </Aux>
-    )
+    render () {
+
+        const ingredientSummary = Object.entries(this.props.ingredients).map(ingred => (
+            <li key={ingred[0]}>
+                <span style={{textTransform: 'capitalize'}}>{ingred[0]}</span>: {ingred[1]}
+            </li>
+        ));
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>An okay burger with these ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button type="Danger" clicked={this.props.clickCancel}>CANCEL</Button>
+                <Button type="Success" clicked={this.props.clickContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
