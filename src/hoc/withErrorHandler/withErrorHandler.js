c9c8b084cbc8f84b617c0b5b06977be7747a39ab
Adam Pierce <adam.pierc3@gmail.com>
2019-01-28 13:48:58 -0800

    code optimizations, removing error handlers on unmounting

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error:null
         }
         componentWillMount () {
-            axios.interceptors.request.use(req => {             //used to set error state to null if there are no errors
+            this.reqInterceptor = axios.interceptors.request.use(req => {             //used to set error state to null if there are no errors
                 this.setState({error: null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {     //record error if there is one
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {     //record error if there is one
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount() {
+            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
