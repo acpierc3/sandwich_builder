3ba4d8ea758c1a5fc706adb734ab3f43e15651ea
Adam Pierce <adam.pierc3@gmail.com>
2019-01-29 14:44:34 -0800

    set up logic for fetching order summary from back-end

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {   //can use this instead of componentdidupdate because we will only be seeing this page when orders is clicked, there will be no updating
+        axios.get('/orders')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            })
+    }
+
     render () {
         return (
             <div>
@@ -13,4 +38,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
