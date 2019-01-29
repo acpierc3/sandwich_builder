ce4a8fe3e90b70ab65f8e536261424a1f62446c5
Adam Pierce <adam.pierc3@gmail.com>
2019-01-29 10:33:15 -0800

    added checkout summary

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.module.css'
+
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope it taste good</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button 
+                type="Danger"
+                clicked>CANCEL</Button>
+            <Button
+                type="Success"
+                clicked>CONTINUE</Button>
+        </div>
+    )
+}
+
+export default checkoutSummary;
