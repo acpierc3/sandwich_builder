ed2746f51a780759674900bb21bea5ed4c5aa2ed
Adam Pierce <adam.pierc3@gmail.com>
2019-02-25 12:07:07 -0800

    build

---
+++ build/precache-manifest.c78be3de2768ceaaa70f3172b4b944a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "699ab79c7a0cd97d2296",
+    "url": "/static/js/main.699ab79c.chunk.js"
+  },
+  {
+    "revision": "33411c33694bb92710c3",
+    "url": "/static/js/1.33411c33.chunk.js"
+  },
+  {
+    "revision": "699ab79c7a0cd97d2296",
+    "url": "/static/css/main.293dba72.chunk.css"
+  },
+  {
+    "revision": "b78f05db20a6c48d27c7c4cbfa84675b",
+    "url": "/index.html"
+  }
+];
