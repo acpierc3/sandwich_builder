b7f63e08a1d6b4e33a5176e4dfc6d26070b652b5
Adam Pierce <adam.pierc3@gmail.com>
2019-05-24 14:17:41 -0700

    updated contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -99,25 +99,7 @@ class ContactData extends Component {
         loading: false
     }
 
-    // onOrder = (event) => {
-    //     event.preventDefault();         //prevents default of sending request and reloading page
-        
-    //     const order = {
-    //         ingredients: this.props.ingreds,
-    //         price: this.props.price,                //in a real setup you would calculate price server-side so as to avoid users manipulating the price
-    //         customer: {
-    //             name: 'NEW',
-    //             address: {
-    //                 street: '21 New Street',
-    //                 zipCode: '12345',
-    //                 country: 'USA'
-    //             },
-    //             email: 'test@test.com'
-    //         },
-    //         deliveryMethod: 'fastest'
-    //     }
-    //     this.props.onOrderBurger(order);
-    // }
+
 
     orderHandler = (event) => {
         event.preventDefault();
