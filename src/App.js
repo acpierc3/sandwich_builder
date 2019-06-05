54d82b9817eb3e2aa42fb34159c1294b28c10a84
Adam Pierce <adam.pierc3@gmail.com>
2019-06-05 10:28:01 -0700

    save tokens to local storage and added auto authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,8 +8,14 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.authCheckState();
+  }
+
   render() {
     return (
       <div>
@@ -26,7 +33,15 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapDispatchToProps = dispatch => {
+  return {
+      authCheckState: () => dispatch(actions.authCheckState()),
+  };
+}
+
+//need to wrap App in withRouter if using connect in App.js to maintain router functionality
+export default withRouter(connect(null, mapDispatchToProps)(App));
 
 
 //routing using "exact" keyword prevents BurgerBuilder from being loaded all the time
