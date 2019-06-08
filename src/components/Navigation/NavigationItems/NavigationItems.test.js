f6a5929f81427954f78e7dedbf5a46662a1baedf
Adam Pierce <adam.pierc3@gmail.com>
2019-06-08 12:37:14 -0700

    added unit tests for navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import { NavLink } from 'react-router-dom';
 import NavigationItems from './NavigationItems';
 
 configure({adapter: new Adapter()});
@@ -21,6 +22,11 @@ describe('<NavigationItems />', () => {
     it('should render three <li> navigation items if authenticated', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find('li')).toHaveLength(3);
+    })
+
+    it('should render logout navlink if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<li><NavLink to='/logout'>Logout</NavLink></li>)).toEqual(true);
     })
 })
 
