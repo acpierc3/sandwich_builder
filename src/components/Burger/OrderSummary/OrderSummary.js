90fd0d132d1ef092ade2719f6f1c16060b36427a
Adam Pierce <adam.pierc3@gmail.com>
2019-06-17 13:39:34 -0700

    converted ordersummary.js and burgeringredient.js to functional components using React Hooks instead of class based using lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Ox/Ox';
 import Button from '../../UI/Button/Button';
 // import classes from './OrderSummary.module.css'
 
-class OrderSummary extends Component {
-    //this could easily be a functional component, should update
+const OrderSummary = props => {
 
-    render () {
+    const ingredientSummary = Object.entries(props.ingredients).map(ingred => (
+        <li key={ingred[0]}>
+            <span style={{textTransform: 'capitalize'}}>{ingred[0]}</span>: {ingred[1]}
+        </li>
+    ));
 
-        const ingredientSummary = Object.entries(this.props.ingredients).map(ingred => (
-            <li key={ingred[0]}>
-                <span style={{textTransform: 'capitalize'}}>{ingred[0]}</span>: {ingred[1]}
-            </li>
-        ));
-
-        return (
-            <Aux>
-                <h3>Your Order</h3>
-                <p>An okay burger with these ingredients:</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
-                <p>Continue to Checkout?</p>
-                <Button type="Danger" clicked={this.props.clickCancel}>CANCEL</Button>
-                <Button type="Success" clicked={this.props.clickContinue}>CONTINUE</Button>
-            </Aux>
-        );
-    }
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>An okay burger with these ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button type="Danger" clicked={props.clickCancel}>CANCEL</Button>
+            <Button type="Success" clicked={props.clickContinue}>CONTINUE</Button>
+        </Aux>
+    );
 };
 
 export default OrderSummary;
